a61ac9d3d42300c8329ce9ce7e5f4e19920de995
Artur Pszczółka <artur.pszczolka@xidea.pl>
2015-06-05 18:48:38 +0200

    Added ProductChoiceType

---
+++ Form/Type/ProductChoiceType.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * (c) Xidea Artur Pszczółka <biuro@xidea.pl>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Xidea\Bundle\ProductBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Xidea\Bundle\BaseBundle\Form\DataTransformer\ModelToIdTransformer;
+use Xidea\Component\Product\Loader\ProductLoaderInterface;
+
+/**
+ * Description of RegistrationType
+ *
+ * @author Artur Pszczółka <a.pszczolka@xidea.pl>
+ */
+class ProductChoiceType extends AbstractType
+{
+    /*
+     * var ProductLoaderInterface
+     */
+    protected $loader;
+
+    /**
+     * @param ProductLoaderInterface $loader The Product loader
+     */
+    public function __construct(ProductLoaderInterface $loader)
+    {
+        $this->loader = $loader;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $transformer = new ModelToIdTransformer($this->loader);
+        $builder->addModelTransformer($transformer, true);
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'choices' => $this->getChoices()
+        ));
+    }
+    
+    public function getParent()
+    {
+        return 'choice';
+    }
+
+    public function getName()
+    {
+        return 'xidea_product_choice';
+    }
+    
+    protected function getChoices()
+    {
+        $result = array();
+        
+        $products = $this->loader->loadAll();
+        foreach($products as $product) {
+            $result[$product->getId()] = $product->getName();
+        }
+        
+        return $result;
+    }
+}
